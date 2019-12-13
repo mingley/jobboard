be797a034e2f16aa5b972e3ee4b568159d5bc352
mikasd <mikasd@pop-os.localdomain>
2019-12-13 15:51:50 -0800

    set up basic fetch with testing and pasted cron job boilerplate

---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+const baseURL = 'https://jobs.github.com/positions.json'
+
+async function fetchGitHub(){
+
+    let resultCount = 1, onPage = 0;
+    const allJobs = [];
+
+    while(resultCount > 0 ){
+        const res = await fetch(`${baseURL}?page=${onPage}`);
+        const jobs = await res.json();
+        allJobs.push(...jobs);
+        resultCount = jobs.length;
+        console.log('got this many jobs -> ' + jobs.length);
+        onPage++;
+    }
+
+    console.log('allJobs length = ' + allJobs.length);
+}
+
+module.exports = fetchGitHub;
+
+module.exports();
