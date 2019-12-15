6c7962d100647253331f4587bcc254d8959ee304
mikasd <michael.ingley@gmail.com>
2019-12-15 01:28:02 -0800

    setup api, started importing to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Jobs from "./Jobs";
+import fetch from 'node-fetch';
+
+const job_api_url = 'http://localhost:3001/jobs'
 
 const mockjobs = [
   {title: 'SWE 1', company: 'Google'},
@@ -8,7 +11,22 @@ const mockjobs = [
   {title: 'SWE 1', company: 'Apple'},
 ]
 
+async function fetchJobs(updateCb) {
+  const res = await fetch(job_api_url);
+  let json = await res.json();
+
+  updateCb(json);
+  console.log({json})
+}
+
 function App() {
+
+  const [jobList, updateJobs ] = React.useState([]); 
+
+  React.useEffect(() => {
+    fetchJobs(updateJobs);
+  }, [])
+
   return (
     <div className="App">
       <Jobs jobs={mockjobs} />
