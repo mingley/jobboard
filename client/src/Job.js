7163ed34be9403374bdea62b654c7487c219e75f
mikasd <michael.ingley@gmail.com>
2019-12-15 23:49:35 -0800

    styled job listings and started pagination

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,10 +1,20 @@
 import React from "react";
+// import Paper from "@material-ui/core/Paper";
+import { Typography, Paper } from "@material-ui/core";
 
 export default function Job({job}){
+
     return(
-        <div className='job'>
-            {job.title}
-            {job.company}
-        </div>
+        <Paper className='job'>
+            <div>
+                <Typography variant='h5'>{job.title}</Typography>
+                <Typography variant='h6'>{job.company}</Typography>
+                <Typography>{job.location}</Typography>
+            </div>
+            <div>
+                <Typography>{job.created_at.split(' ').slice(0,3).join(' ')}</Typography>
+            </div>
+        </Paper>
     );
+
 }
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import Button from "@material-ui/core/Button";
+
+import MobileStepper from "@material-ui/core/MobileStepper";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+
+
 import Job from "./Job";
 
 export default function Jobs({jobs}){
+
+    const [activeStep, setActiveStep] = useState(0);
+
+    function handleNext(){
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    }
+
+    function handleBack(){
+        setActiveStep(prevActiveStep => prevActiveStep -1);
+    }
+
     return(
-        <div className="jobs-list">
-          <Typography variant="h1">
+        <div className="jobs">
+          <Typography variant="h4" component="h1">
               Entry Level Software Jobs
           </Typography>
           {
               jobs.map(
-                  job => <Job job={job} />
+                  (job, i) => <Job key={i}job={job} />
               )
           }
+
+        <MobileStepper
+            variant="progress"
+            steps={6}
+            position="static"
+            activeStep={activeStep}
+            nextButton={
+                <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+                Next
+                <KeyboardArrowLeft /><KeyboardArrowRight />
+                </Button>
+            }
+            backButton={
+                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                <KeyboardArrowRight /><KeyboardArrowLeft />
+                Back
+                </Button>
+            }
+            />
         </div>
     )
 };
